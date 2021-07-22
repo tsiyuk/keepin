5fee842f0f95785718b3aa1adda933cd20a8a297
YaleChen299 <proverb299@gmail.com>
2021-07-22 18:44:23 +0800

    postPage

---
+++ lib/pages/Post/LikeCommentShare.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:keepin/src/CommonWidgets.dart';
+import 'package:keepin/src/models/Post.dart';
+import 'package:keepin/src/services/PostProvider.dart';
+import 'package:provider/provider.dart';
+
+class LikeCommentShare extends StatefulWidget {
+  final Post post;
+  LikeCommentShare({required this.post});
+  @override
+  _LikeCommentShareState createState() => _LikeCommentShareState();
+}
+
+class _LikeCommentShareState extends State<LikeCommentShare> {
+  final double iconSize = 20;
+  bool hasLiked = false;
+  num numOfLikes = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void setState(fn) {
+    if (mounted) {
+      super.setState(fn);
+    }
+  }
+
+  void initPost() async {
+    //postProvider.loadAll(widget.post);
+    final bool temp = await PostProvider.hasLiked(widget.post);
+    setState(() {
+      hasLiked = temp;
+      numOfLikes = widget.post.numOfLikes;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    PostProvider postProvider = Provider.of<PostProvider>(context);
+    initPost();
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        TextButton(
+          style: TextButton.styleFrom(primary: Colors.grey),
+          child: Container(
+            child: Row(
+              children: [
+                hasLiked
+                    ? Icon(Icons.favorite_rounded,
+                        color: Colors.red.shade200, size: iconSize)
+                    : Icon(Icons.favorite_border_rounded, size: iconSize),
+                Text(" " + numOfLikes.toString())
+              ],
+            ),
+          ),
+          onPressed: () {
+            if (hasLiked) {
+              postProvider.unlikeViaPost(widget.post);
+            } else {
+              postProvider.likeViaPost(widget.post);
+            }
+          },
+        ),
+        TextButton(
+          style: TextButton.styleFrom(primary: Colors.grey),
+          child: Container(
+            child: Row(
+              children: [
+                Icon(Icons.messenger_outline_rounded, size: iconSize),
+                Text(" comment")
+              ],
+            ),
+          ),
+          onPressed: () {},
+        ),
+        TextButton(
+          style: TextButton.styleFrom(primary: Colors.grey),
+          child: Container(
+            child: Row(
+              children: [
+                Icon(Icons.share_outlined, size: iconSize),
+                Text(" share")
+              ],
+            ),
+          ),
+          onPressed: () {},
+        )
+      ],
+    );
+  }
+}
--- lib/pages/Post/PostPage.dart
+++ lib/pages/Post/PostPage.dart
@@ -1,96 +1,121 @@
+import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
+import 'package:keepin/pages/Post/LikeCommentShare.dart';
 import 'package:keepin/src/CommonWidgets.dart';
 import 'package:keepin/src/models/Post.dart';
+import 'package:keepin/src/models/UserProfile.dart';
 import 'package:keepin/src/services/PostProvider.dart';
+import 'package:keepin/src/services/UserProfileProvider.dart';
 import 'package:provider/provider.dart';
 
-class LikeCommentShare extends StatefulWidget {
+import '../UserProfileDisplay.dart';
+
+class PostPage extends StatefulWidget {
   final Post post;
-  LikeCommentShare({required this.post});
+  PostPage({required this.post});
   @override
-  _LikeCommentShareState createState() => _LikeCommentShareState();
+  _PostPageState createState() => _PostPageState();
 }
 
-class _LikeCommentShareState extends State<LikeCommentShare> {
-  final double iconSize = 20;
-  bool hasLiked = false;
-  num numOfLikes = 0;
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void setState(fn) {
-    if (mounted) {
-      super.setState(fn);
-    }
-  }
-
-  void initPost() async {
-    //postProvider.loadAll(widget.post);
-    final bool temp = await PostProvider.hasLiked(widget.post);
-    setState(() {
-      hasLiked = temp;
-      numOfLikes = widget.post.numOfLikes;
-    });
-  }
-
+class _PostPageState extends State<PostPage> {
+  Widget title = Text("Post Detail");
   @override
   Widget build(BuildContext context) {
-    PostProvider postProvider = Provider.of<PostProvider>(context);
-    initPost();
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      mainAxisSize: MainAxisSize.max,
-      children: [
-        TextButton(
-          style: TextButton.styleFrom(primary: Colors.grey),
-          child: Container(
-            child: Row(
-              children: [
-                hasLiked
-                    ? Icon(Icons.favorite_rounded,
-                        color: Colors.red.shade200, size: iconSize)
-                    : Icon(Icons.favorite_border_rounded, size: iconSize),
-                Text(" " + numOfLikes.toString())
-              ],
-            ),
-          ),
-          onPressed: () {
-            if (hasLiked) {
-              postProvider.unlikeViaPost(widget.post);
-            } else {
-              postProvider.likeViaPost(widget.post);
-            }
-          },
+    return Scaffold(
+      appBar: AppBar(
+        title: title,
+      ),
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Column(
+          children: [
+            _buildPoster(context, widget.post),
+            _buildPost(context),
+            _buildImages(context, widget.post),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
+              child: LikeCommentShare(post: widget.post),
+            )
+          ],
         ),
-        TextButton(
-          style: TextButton.styleFrom(primary: Colors.grey),
-          child: Container(
+      ),
+    );
+  }
+
+  Widget _buildPoster(BuildContext context, Post post) {
+    UserProfileProvider userProfileProvider =
+        Provider.of<UserProfileProvider>(context);
+    return FutureBuilder<UserProfile>(
+      future: userProfileProvider.readUserProfile(post.posterId),
+      builder: (context, snapshot) {
+        if (snapshot.data != null) {
+          return Container(
+            padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 0),
             child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Icon(Icons.messenger_outline_rounded, size: iconSize),
-                Text(" comment")
+                ImageButton(
+                  image: post.posterAvatarLink == null
+                      ? defaultAvatar(50)
+                      : Image.network(
+                          post.posterAvatarLink!,
+                          fit: BoxFit.cover,
+                        ),
+                  size: 50,
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) =>
+                            UserProfileDisplay(post.posterId)));
+                  },
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextH3(
+                      post.posterName,
+                      size: 20,
+                    ),
+                    TextH4(snapshot.data!.bio ?? "")
+                  ],
+                ),
               ],
             ),
-          ),
-          onPressed: () {},
+          );
+        } else {
+          return SizedBox();
+        }
+      },
+    );
+  }
+
+  Widget _buildPost(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 14),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).primaryColorLight.withOpacity(0.3),
+          borderRadius: BorderRadius.circular(4),
         ),
-        TextButton(
-          style: TextButton.styleFrom(primary: Colors.grey),
-          child: Container(
-            child: Row(
-              children: [
-                Icon(Icons.share_outlined, size: iconSize),
-                Text(" share")
-              ],
-            ),
+        padding: const EdgeInsets.all(16),
+        child: Text(
+          widget.post.text,
+          style: TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.w400,
+            color: Colors.black87,
           ),
-          onPressed: () {},
-        )
-      ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildImages(BuildContext context, Post post) {
+    return Wrap(
+      children: post.imageLinks.map((element) {
+        return ImageButton(image: Image.network(element), size: 60);
+      }).toList(),
     );
   }
 }
